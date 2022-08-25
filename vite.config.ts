b03a2fe5866f38c9b15d21e6c560da77ad4c16d1
surilindur <surilindur@users.noreply.github.com>
2022-08-25 16:53:23 +0200

    changed to use yarn

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
   },
   optimizeDeps: {
     esbuildOptions: {
+      target: 'esnext',
       plugins: [NodeGlobalsPolyfillPlugin({ process: true, buffer: true }), NodeModulesPolyfillPlugin()]
     }
   },
